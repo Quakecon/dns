1e4539a922dcce44e01975b2a8d8e8c1d9e9791c
Shawn Nock <nock@nocko.se>
2016-07-21 13:07:25 -0400

    Update post-commit to intelligently run outside of git hook

--- scripts/gen-secret.sh
+++ scripts/gen-secret.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+set -u
+set -e
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <TSIG template file>"
+fi
+
+FILENAME=$1
+
 TSIG=$(dd if=/dev/random of=/dev/stdout count=1 bs=32 | base64)
 
-sed -e "s,{{ SECRET }},${TSIG}," secret.keys.template > secret.keys
+sed -e "s,{{ SECRET }},${TSIG}," $FILENAME
